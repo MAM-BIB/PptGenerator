78e004d678587245235969223013614161b27bd7
Marco Kühn <marco.kuehn2000@gmail.com>
2022-02-21 16:06:57 +0100

    Projekt setup

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+app.on('ready', () => {
+    console.log('App is ready');
+
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: true,
+        }
+    });
+
+    const indexHTML = path.join(__dirname, 'views/index.html');
+    win.loadFile(indexHTML).then(() => {
+        // IMPLEMENT FANCY STUFF HERE
+    }).catch(e => console.error(e));
+});
