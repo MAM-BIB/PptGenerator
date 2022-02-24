63d831840c681d084798d8ca09cb33e975536e04
Marc Beyer <marc.beyer.dev@gmail.com>
2022-02-24 16:24:47 +0100

    Added cla

---
+++ backend/PptGenerator/CommandLine/CommandLineArgumentParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PptGenerator.CommandLine {
+    class CommandLineArgumentParser {
+        public static void Parse(string[] args) {
+            List<string> argList = new List<string>(args);
+
+            Mode mode = Mode.scan;
+
+            int modeIndex = argList.IndexOf("-mode");
+            if (modeIndex != -1 && modeIndex - 1 <= argList.Count) {
+                try {
+                    mode = (Mode)Enum.Parse(typeof(Mode), argList[modeIndex + 1], true);
+                } catch (Exception e) {
+                    Console.WriteLine("the mode is not defined correct so mode is set to 'scan'");
+                }
+            } else {
+                Console.WriteLine("the mode is not defined so mode is set to 'scan'");
+            }
+        }
+    }
+}
