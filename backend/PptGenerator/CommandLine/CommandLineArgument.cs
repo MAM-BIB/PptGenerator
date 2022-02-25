043d2c88206a7ae98f2fa7415223c8c9645871df
Marco Kühn <marco.kuehn2000@gmail.com>
2022-02-25 10:21:42 +0100

    Added basePath and deleteFirstSlide

--- backend/PptGenerator/CommandLine/CommandLineArgument.cs
+++ backend/PptGenerator/CommandLine/CommandLineArgument.cs
@@ -17,19 +17,33 @@ namespace PptGenerator.CommandLine {
         private List<string> _inPaths;
         private List<uint> _slidePos;
         private bool _ignoreTheme;
+        private bool _deleteFirstSlide;
+        private string _basePath;
 
         public Mode Mode { get => _mode; }
         public string OutPath { get => _outPath; }
         public List<string> InPaths { get => _inPaths; }
         public List<uint> SlidePos { get => _slidePos; }
         public bool IgnoreTheme { get => _ignoreTheme; }
+        public bool DeleteFirstSlide { get => _deleteFirstSlide; }
+        public string BasePath { get => _basePath; }
 
-        public CommandLineArgument(Mode mode, string outPath, List<string> inPaths, List<uint> slidePos = null, bool ignoreTheme = false) {
+        public CommandLineArgument(
+            Mode mode,
+            string outPath,
+            List<string> inPaths,
+            List<uint> slidePos = null,
+            bool ignoreTheme = false,
+            bool deletFirstSlide = false,
+            string basePath = null
+         ) {
             _mode = mode;
             _outPath = outPath;
             _inPaths = inPaths;
             _slidePos = slidePos;
             _ignoreTheme = ignoreTheme;
+            _deleteFirstSlide = deletFirstSlide;
+            _basePath = basePath;
         }
     }
 }
