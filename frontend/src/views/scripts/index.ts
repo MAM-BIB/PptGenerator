ea4bc13b359065725c9adead64d5943c3d000b87
Marco Kühn <marco.kuehn2000@gmail.com>
2022-03-03 14:10:06 +0100

    Improved uids

--- frontend/src/views/scripts/index.ts
+++ frontend/src/views/scripts/index.ts
@@ -26,15 +26,36 @@ async function read() {
 
 function createSection(section: Section): HTMLElement {
     const sectionElement = document.createElement("div");
-    sectionElement.classList.add("section");
+    const header = document.createElement("div");
+    const headerText = document.createElement("h2");
 
-    const header = document.createElement("h2");
-    sectionElement.append(header);
-    header.textContent = `${section.Name} (${section.Slides.length})`;
-    header.title = `show/hide slides of ${section.Name}`;
-    header.addEventListener("click", () => {
+    sectionElement.classList.add("section");
+    headerText.classList.add("headerText");
+    header.classList.add("sectionHeader");
+
+    headerText.textContent = `${section.Name} (${section.Slides.length})`;
+
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("sectionButtons");
+
+    const buttonCollapse = document.createElement("button");
+    buttonCollapse.textContent = "▼";
+    buttonCollapse.title = `show/hide slides of ${section.Name}`;
+    buttonCollapse.classList.add("collapseSection");
+    buttonCollapse.addEventListener("click", () => {
         sectionElement.classList.toggle("open");
     });
+    buttonContainer.append(buttonCollapse);
+
+    const buttonSelect = document.createElement("button");
+    buttonSelect.title = `select all slides of ${section.Name}`;
+    buttonSelect.classList.add("selectSection");
+    buttonSelect.textContent = "+";
+    buttonContainer.append(buttonSelect);
+
+    header.append(headerText);
+    header.append(buttonContainer);
+    sectionElement.append(header);
 
     for (const slide of section.Slides) {
         sectionElement.append(createSlide(slide));
