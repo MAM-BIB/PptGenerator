719f120648a2169965e8320f21de007c28e4fba2
Alex Rechtin <alexrechtin@gmail.com>
2022-03-04 10:17:05 +0100

    Created the option window

--- frontend/src/menu.ts
+++ frontend/src/menu.ts
@@ -1,4 +1,5 @@
 import { app, BrowserWindow, Menu } from "electron";
+import path from "path";
 
 export default function initMenu(browserWindow: BrowserWindow) {
     const menu = Menu.buildFromTemplate([
@@ -36,9 +37,38 @@ export default function initMenu(browserWindow: BrowserWindow) {
                         app.quit();
                     },
                 },
+                {
+                    label: "Option",
+                    accelerator: "1",
+                    click() {
+                        openOption();
+                    },
+                },
             ],
         },
     ]);
 
+    let optionOpen: boolean;
+
+    function openOption() {
+        if (!optionOpen) {
+            const optionWindow = new BrowserWindow({
+                width: 500,
+                height: 500,
+                minWidth: 500,
+                minHeight: 500,
+                resizable: false,
+                useContentSize: true,
+            });
+            const indexHTML = path.join(__dirname, "views/option.html");
+            optionWindow.loadFile(indexHTML).catch((error) => {
+                console.log(error);
+            });
+            optionOpen = true;
+            optionWindow.on("close", () => {
+                optionOpen = false;
+            });
+        }
+    }
     Menu.setApplicationMenu(menu);
 }
