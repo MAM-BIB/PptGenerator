f6ecb2e2286bb3e6d24724cd30d95cdc8621f870
Marc Beyer <marc.beyer.dev@gmail.com>
2022-03-04 09:21:26 +0100

    Added config

--- frontend/src/menu.ts
+++ frontend/src/menu.ts
@@ -1,4 +1,6 @@
 import { app, BrowserWindow, Menu } from "electron";
+import { spawn } from "child_process";
+import getConfig from "./config";
 
 export default function initMenu(browserWindow: BrowserWindow) {
     const menu = Menu.buildFromTemplate([
@@ -34,6 +36,33 @@ export default function initMenu(browserWindow: BrowserWindow) {
                     accelerator: "Alt+F4",
                     click() {
                         app.quit();
+                    },
+                },
+                {
+                    label: "Scan Presentation",
+                    click() {
+                        console.log("");
+                        const bat = spawn(
+                            "C:/Users/bib/Projects/Git/PptGenerator/backend/PptGenerator/bin/Release/netcoreapp3.1/PptGenerator.exe",
+                            ["-inPath", "../backend/slides/All_slides_EN_small.pptx", "-outPath", "./test.json"],
+                        );
+                        bat.stdout.on("data", (data) => {
+                            console.log(data.toString());
+                        });
+
+                        bat.stderr.on("data", (data) => {
+                            console.error(data.toString());
+                        });
+
+                        bat.on("exit", (code) => {
+                            console.log(`Child exited with code ${code}`);
+                        });
+                    },
+                },
+                {
+                    label: "getConfig",
+                    click() {
+                        getConfig();
                     },
                 },
             ],
