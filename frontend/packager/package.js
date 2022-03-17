8c22bbc920b86494a3d9f263e31297dee07115ce
Marco Kühn <marco.kuehn2000@gmail.com>
2022-03-17 12:16:36 +0100

    First draft to minify js

--- frontend/packager/package.js
+++ frontend/packager/package.js
@@ -1,5 +1,8 @@
 const packager = require("electron-packager");
+const path = require("path");
 const fs = require("fs");
+const uglifyJs = require("uglify-js");
+const archiver = require("archiver");
 
 async function bundleElectronApp(options) {
     const appPaths = await packager(options);
@@ -27,4 +30,55 @@ async function bundleElectronApp(options) {
     fs.cpSync("../backend/PptGenerator/bin/Release/netcoreapp3.1/", "../dist/pptgenerator-win32-x64/core/", {
         recursive: true,
     });
+
+    // minify("../dist/");
 })();
+
+function minify(dir) {
+    const foundFiles = [];
+    const files = fs.readdirSync(dir);
+    for (const file of files) {
+        if (file.isDirectory()) {
+            minify(file.name);
+        } else {
+            foundFiles.push(file);
+        }
+    }
+    uglify(foundFiles);
+}
+
+function uglify(files) {
+    for (const file of files) {
+        if (path.extname(file) === ".js") {
+            let options = {
+                mangle: {
+                    properties: true,
+                },
+                nameCache: JSON.parse(fs.readFileSync(file, "utf8")),
+            };
+            fs.writeFileSync(
+                "part1.js",
+                UglifyJS.minify(
+                    {
+                        "file1.js": fs.readFileSync("file1.js", "utf8"),
+                        "file2.js": fs.readFileSync("file2.js", "utf8"),
+                    },
+                    options,
+                ).code,
+                "utf8",
+            );
+            fs.writeFileSync(
+                "part2.js",
+                UglifyJS.minify(
+                    {
+                        "file3.js": fs.readFileSync("file3.js", "utf8"),
+                        "file4.js": fs.readFileSync("file4.js", "utf8"),
+                    },
+                    options,
+                ).code,
+                "utf8",
+            );
+            fs.writeFileSync(cacheFileName, JSON.stringify(options.nameCache), "utf8");
+        }
+    }
+}
