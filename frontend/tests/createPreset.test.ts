83fcc49cefa3db8a553eb6f6bcb4b10a59bb3bb0
Marco Kühn <marco.kuehn2000@gmail.com>
2022-03-23 15:29:29 +0100

    Added create preset tests

---
+++ frontend/tests/createPreset.test.ts
@@ -0,0 +1,142 @@
+import fs from "fs";
+import path from "path";
+import { Placeholder, Presentation, Preset, PresetSection, Section, Slide } from "../src/scripts/interfaces/interfaces";
+import createPreset from "../src/scripts/ui/components/createPreset";
+
+const tmpPath = "./tests/files/tmp";
+const presetPath = path.join(tmpPath, "preset.json");
+
+const slide1: Slide = {
+    Title: "Test1",
+    Uid: "4321Test1",
+    RelationshipId: "RelationshipId",
+    Position: 42,
+    IsHidden: false,
+    IsSelected: true,
+    Placeholders: [],
+};
+
+const slide2: Slide = {
+    Title: "Test2",
+    Uid: "4321Test2",
+    RelationshipId: "RelationshipId2",
+    Position: 43,
+    IsHidden: true,
+    IsSelected: false,
+    Placeholders: ["Name"],
+};
+
+const slide3: Slide = {
+    Title: "Test3",
+    Uid: "4321Test3",
+    RelationshipId: "RelationshipId3",
+    Position: 44,
+    IsHidden: true,
+    IsSelected: true,
+    Placeholders: [],
+};
+
+const slide4: Slide = {
+    Title: "Test4",
+    Uid: "4321Test4",
+    RelationshipId: "RelationshipId4",
+    Position: 45,
+    IsHidden: false,
+    IsSelected: false,
+    Placeholders: [],
+};
+
+const slide5: Slide = {
+    Title: "Test5",
+    Uid: "4321Test5",
+    RelationshipId: "RelationshipId5",
+    Position: 46,
+    IsHidden: false,
+    IsSelected: false,
+    Placeholders: [],
+};
+
+const section1: Section = {
+    Name: "TestSection1",
+    Slides: [slide1, slide2],
+};
+
+const section2: Section = {
+    Name: "TestSection2",
+    Slides: [slide3, slide4],
+};
+
+const section3: Section = {
+    Name: "TestSection2",
+    Slides: [slide5],
+};
+
+const presentation1: Presentation = {
+    Path: "C:/TestPath/test1.pptx",
+    Sections: [section1, section2, section3],
+};
+
+const placeholder1: Placeholder = {
+    name: "Name",
+    value: "name",
+};
+
+const presentations: Presentation[] = [presentation1];
+const placeholders1: Placeholder[] = [placeholder1];
+const placeholders2: Placeholder[] = [];
+
+function clearTmpFolder() {
+    if (fs.existsSync(tmpPath)) {
+        if (fs.readdirSync(tmpPath).length) {
+            fs.rmSync(tmpPath, { recursive: true });
+        }
+    }
+    fs.mkdirSync(tmpPath, { recursive: true });
+}
+
+beforeEach(() => {
+    clearTmpFolder();
+});
+
+afterAll(() => {
+    clearTmpFolder();
+});
+
+test("create a preset.json file", async () => {
+    await createPreset(presetPath, presentations, placeholders1);
+    expect(fs.existsSync(presetPath)).toBe(true);
+});
+
+test("create preset.json without placeholders", async () => {
+    await createPreset(presetPath, presentations, placeholders2);
+    const preset = await JSON.parse(fs.readFileSync(presetPath, { encoding: "utf-8" }));
+    expect(preset).toEqual({
+        path: presetPath,
+        placeholders: [],
+        sections: [
+            { ignoredSlides: ["4321Test2"], includedSlides: ["4321Test1"], name: "TestSection1" },
+            { ignoredSlides: ["4321Test4"], includedSlides: ["4321Test3"], name: "TestSection2" },
+        ],
+    });
+});
+
+test("create preset.json with placeholders", async () => {
+    await createPreset(presetPath, presentations, placeholders1);
+    const preset = await JSON.parse(fs.readFileSync(presetPath, { encoding: "utf-8" }));
+    expect(preset).toEqual({
+        path: presetPath,
+        placeholders: [{ name: "Name", value: "name" }],
+        sections: [
+            {
+                ignoredSlides: ["4321Test2"],
+                includedSlides: ["4321Test1"],
+                name: "TestSection1",
+            },
+            {
+                ignoredSlides: ["4321Test4"],
+                includedSlides: ["4321Test3"],
+                name: "TestSection2",
+            },
+        ],
+    });
+});
