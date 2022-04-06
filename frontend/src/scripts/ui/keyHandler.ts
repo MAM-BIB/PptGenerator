bff508553bbb9ea263e20d8da77e1c3e528fd3ee
Marc Beyer <marc.beyer.dev@gmail.com>
2022-04-06 12:10:40 +0200

    Fixed zoom

--- frontend/src/scripts/ui/keyHandler.ts
+++ frontend/src/scripts/ui/keyHandler.ts
@@ -1,11 +1,9 @@
 import { getConfig, setConfig } from "../helper/config";
 
-const config = getConfig();
 // Adds event listener if the shift key is pressed
 let shiftPressed = false;
 
 let ctrlPressed = false;
-let zoom = config.imgZoom;
 
 document.addEventListener("keydown", (event) => {
     if (event.key === "Shift") {
@@ -33,9 +31,16 @@ document.addEventListener("keydown", (event) => {
     }
 });
 
+document.addEventListener("focusout", () => {
+    shiftPressed = false;
+    ctrlPressed = false;
+    setConfig(getConfig());
+});
+
 document.addEventListener("keyup", (event) => {
     if (event.key === "Control") {
         ctrlPressed = false;
+        setConfig(getConfig());
     }
 });
 
@@ -43,20 +48,18 @@ export function addZoomListener() {
     setImgSize();
     document.addEventListener("wheel", (event) => {
         if (ctrlPressed) {
-            zoom = Math.min(Math.max(zoom + event.deltaY * -0.1, 0), 100);
+            getConfig().imgZoom = Math.min(Math.max(getConfig().imgZoom + event.deltaY * -0.1, 0), 100);
             setImgSize();
-            config.imgZoom = zoom;
-            setConfig(config);
         }
     });
 }
 
 function setImgSize() {
     const root = document.documentElement;
-    if (zoom < 10) {
+    if (getConfig().imgZoom < 10) {
         root.style.setProperty("--hide", "none");
     } else {
         root.style.setProperty("--hide", "block");
     }
-    root.style.setProperty("--zoom", `${zoom}%`);
+    root.style.setProperty("--zoom", `${getConfig().imgZoom}%`);
 }
