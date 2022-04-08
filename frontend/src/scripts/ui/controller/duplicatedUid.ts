32029837fdce1cc27be3ed42b6f5ae3e1874d00b
Marco Kühn <marco.kuehn2000@gmail.com>
2022-04-08 09:54:57 +0200

    Refactor imageLoader loation

--- frontend/src/scripts/ui/controller/duplicatedUid.ts
+++ frontend/src/scripts/ui/controller/duplicatedUid.ts
@@ -9,7 +9,7 @@ import initTitlebar from "../components/titlebar";
 import { getConfig } from "../../helper/config";
 import { startLoading } from "../components/loading";
 import openPopup from "../../helper/openPopup";
-import checkForImg from "./imageLoader";
+import checkForImg from "../../helper/imageLoader";
 
 const duplicatedUidSection = document.getElementById("duplicated-uid-section") as HTMLDivElement;
 const cancelBtn = document.getElementById("cancel-btn") as HTMLButtonElement;
